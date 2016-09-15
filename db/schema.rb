a477ccb38e7effc82f62175dd6aa4b8a08e9597c
Claudia McCormack <mccormack.chc@gmail.com>
2016-09-15 19:53:15 -0400

    added student dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160915230301) do
 
   # These are extensions that must be enabled in order to support this database
@@ -35,12 +34,6 @@ ActiveRecord::Schema.define(version: 20160915230301) do
     t.datetime "updated_at",      null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string   "skill_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "experiences", force: :cascade do |t|
     t.date     "start_date"
     t.date     "end_date"
@@ -49,6 +42,12 @@ ActiveRecord::Schema.define(version: 20160915230301) do
     t.text     "details"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "skill_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
