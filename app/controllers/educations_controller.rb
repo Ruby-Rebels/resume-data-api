2cbcaa586408271b2ba1a7896f90ebcc37123d1e
brandonbaah <brandonbaah@gmail.com>
2016-09-15 21:25:30 -0400

    added education views and controllers

---
+++ app/controllers/educations_controller.rb
@@ -0,0 +1,38 @@
+class EducationsController < ApplicationController
+  def index
+    @education = Education.all
+  end
+
+  def new
+  end
+
+  def show
+    @education = Education.find_by(id: params[:id])
+  end
+
+  def create
+    Education.create(
+    start_date: params[:start_date],
+    end_date: params[:end_date],
+    degree: params[:degree],
+    university_name: params[:university_name],
+    details: params[:details]
+    )
+  end
+
+  def update
+    @education = Education.find_by(id: params[:id])
+    Education.update(
+    start_date: params[:start_date],
+    end_date: params[:end_date],
+    degree: params[:degree],
+    university_name: params[:university_name],
+    details: params[:details]
+    )
+  end
+
+  def destroy
+    @education = Education.find_by(id: params[:id])
+    @education.destroy
+  end
+end
---
+++ app/views/educations/_education.json.jbuilder
@@ -0,0 +1,7 @@
+json.id education.id
+json.start_date education.start_date
+json.end_date education.end_date
+json.degree education.degree
+json.email student.email
+json.university_name education.university_name
+json.details education.details
