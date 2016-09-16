08ce8be6e20d617f68ca62cd4f266fe389167f8a
Andy Zhao <andyzhao.zhao@gmail.com>
2016-09-15 21:16:20 -0400

    added experience controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
+      get '/students/experience' => 'experience#show'
+      get '/students/experience/:id' => 'experience#show'
+      post '/students/experience' => 'experience#create'
+      patch '/students/experience' => 'experience#update'
+      delete '/students/experience' => 'experience#destroy'
+
       get '/students' => 'students#index'
       get '/students/:id' => 'students#show'
       post '/students' => 'students#create'
