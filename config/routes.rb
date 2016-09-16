8a34410fed56211d4638f8356ed494b86b02407c
Claudia McCormack <mccormack.chc@gmail.com>
2016-09-15 20:39:55 -0400

    started students controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/students' => 'students#index'
+      get '/students/:id' => 'students#show'
+      post '/students' => 'students#create'
+      post '/students/:id' => 'students#update'
+      delete '/students/:id' => 'students#destroy'
     end
   end
 end
